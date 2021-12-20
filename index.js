c701c48ad771c3f64105e4fad74eb9f6992f757b
BertrandConxy <90222110+BertrandConxy@users.noreply.github.com>
2021-12-20 18:00:27 +0200

    Add hamburger menu functionality

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-let hamburger = document.querySelector('.hamburger');
+let hamburger = document.querySelector('#hamburger');
+let closetog = document.querySelector('#togclose');
 let toggleMenu = document.querySelector('.toggle-menu');
 
 hamburger.addEventListener('click', show)
-toggleMenu.addEventListener('click', close)
+closetog.addEventListener('click', close)
+
+function show() {
+    toggleMenu.style.display ='flex';
+    toggleMenu.style.top ='0';
+
+}
+
+function close () {
+    toggleMenu.style.top ='-100%';
+}
