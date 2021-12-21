b2bde2dbf998cecf6a8b683131206738c39af1fa
Yerimah <danielyerimah@yahoo.com>
2021-12-21 15:46:24 +0100

    Store project information using javascript arrays

--- index.js
+++ index.js
@@ -15,3 +15,76 @@ hamburger.addEventListener('click', show);
 closetog.addEventListener('click', close);
 
 document.querySelectorAll('.menu-list').forEach((link) => link.addEventListener('click', close));
+
+let cardWorks = [{ 
+    id: "1",
+    name: "Multi-Post Stories",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    technologies: ['css', 'html', 'bootstrap', 'Ruby'],
+    image: "./images/placeholder.png", alt: "first project image",
+    liveLink: "#",
+    sourceLink: "#"
+},
+
+ {
+     id: "2",
+     name: "Professional Art",
+     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+     technologies: ['html', 'bootstrap', 'Ruby'],
+     image: "background: url(./images/portfolio.png)",
+     liveLink: "#",
+     sourceLink: "#"
+ },
+
+ {
+    id: "3",
+    name: "Professional Art",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    image: "background: url(./images/portfolio.png)",
+    liveLink: "#",
+    sourceLink: "#"
+ },
+
+ {
+    id: "4",
+    name: "Professional Art",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    image: "background: url(./images/portfolio.png)",
+    liveLink: "#",
+    sourceLink: "#"
+ },
+
+ {
+    id: "5",
+    name: "Professional Art",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    image: "background: url(./images/portfolio.png)",
+    liveLink: "#",
+    sourceLink: "#"
+ },
+
+ {
+    id: "6",
+    name: "Professional Art",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    image: "background: url(./images/portfolio.png)",
+    liveLink: "#",
+    sourceLink: "#"
+ },
+
+ {
+    id: "7",
+    name: "Professional Art",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    image: "background: url(./images/portfolio.png)",
+    liveLink: "#",
+    sourcelink: "#"
+    
+ }
+
+]
